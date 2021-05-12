005c144cb7f1b9096d7dc5a077aafccbe4ec088a
Tim Maas Geesteranus <tim__mg@hotmail.com>
2021-05-12 12:27:38 +0200

    add a normal class

---
+++ MyMountainAscents.UI/MyMountainAscents.UI/Components/MountainCard.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMountainAscents.UI.Components
+{
+    public partial class MountainCard
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+    }
+}
