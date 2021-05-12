7297ffd6fec5b9d24646e43f903c619f284cbc18
Tim Maas Geesteranus <tim__mg@hotmail.com>
2021-05-12 12:40:26 +0200

    Add entities

---
+++ MyMountainAscents.UI/MyMountainAscents.API/Entities/Ascent.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyMountainAscents.API.Entities
+{
+    public class Ascent
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public Mountain Mountain { get; set; }
+
+        [Required]
+        public string Date { get; set; }
+
+        public string Comment { get; set; }
+
+    }
+}
---
+++ MyMountainAscents.UI/MyMountainAscents.API/Entities/Mountain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyMountainAscents.API.Entities
+{
+    public class Mountain
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public int Height { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+        public List<Ascent> Ascents { get; set; }
+    }
+}
